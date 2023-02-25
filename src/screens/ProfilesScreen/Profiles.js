a27f2513b1582ab5dd1fb36a801966e6ae3e66aa
Alperen Kapusuz <sefalperen21@gmail.com>
2023-02-26 01:06:32 +0300

    Profiles detail fetched

--- src/screens/ProfilesScreen/Profiles.js
+++ src/screens/ProfilesScreen/Profiles.js
@@ -4,15 +4,16 @@ import backgr from '../../../assets/images/arkaplan.png';
 import {useNavigation} from '@react-navigation/native';
 import React from 'react'
 import { useGetMe } from '../../api/auth';
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import * as Keychain from 'react-native-keychain'
 import { resetAuth } from '../../store/authStore';
 
 const Profiles = () => {
-  const dispatch = useDispatch()
-  const navigation = useNavigation();
-  const {data:me} = useGetMe()
-  console.log(me)
+  const {myDetails} = useSelector((state) => state.authStore)
+  // const {data:me} = useGetMe() //Çalışmamakta
+  // console.log(me) 
+  //<Avatar.Text size={90} label="TG"/> şimdilik bu şekilde kalsın
+  console.log(myDetails)
 
   const logout = async () => {
     await Keychain.resetGenericPassword()
@@ -34,11 +35,11 @@ const Profiles = () => {
       <Avatar.Text size={90} label="TG"/>
       </Card.Content>
       <Card.Content style={{flexDirection:'row', alignItems: 'center', width: '60%', justifyContent:'space-evenly'}}>
-        <Text style={{fontSize: 20, fontFamily: 'Inter-Medium', color:'#000000'}}>Tomioka</Text>
-        <Text style={{fontSize: 20, fontFamily: 'Inter-Medium', color:'#000000'}}>Giyuu</Text>
+        <Text style={{fontSize: 20, fontFamily: 'Inter-Medium', color:'#000000'}}>{myDetails.name}</Text>
+        <Text style={{fontSize: 20, fontFamily: 'Inter-Medium', color:'#000000'}}>{myDetails.surname}</Text>
       </Card.Content>
       <Card.Content style={{ alignItems: 'center', marginTop:10}}>
-      <Text style={{fontSize: 15, fontFamily: 'Inter-Regular', color:'#000000'}}>tomiokaGiyuu@test.com</Text>
+      <Text style={{fontSize: 15, fontFamily: 'Inter-Regular', color:'#000000'}}>{myDetails.mail}</Text>
       </Card.Content>
     </Card>
     <Card 
