5af8d26cece3717c297e7cdb61bf13bd19a62d9b
Alperen Kapusuz <sefalperen21@gmail.com>
2023-02-25 21:02:49 +0300

    register api connected

--- src/api/auth.js
+++ src/api/auth.js
@@ -15,4 +15,20 @@ const postLogin = async (values={username, password}) => {
 export const useLogin = () => {
     return useMutation(postLogin)
 }
-//=============================================
+
+//================= REGISTER ======================
+const postRegister = async (values = {username, mail, password, name, surname}) => {
+    const userData = {
+        username: values.username,
+        mail: values.mail,
+        password: values.password,
+        name: values.name,
+        surname: values.surname,
+    }
+    const response = await axios.post(END_POINTS.AUTH_CONTROLLER.REGISTER, userData)
+    return response.data
+}
+
+export const useRegister = () => {
+    return useMutation(postRegister)
+}
