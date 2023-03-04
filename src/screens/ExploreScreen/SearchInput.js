bb0dab4f30765738959677c81ea3167b0a85fd38
Alperen Kapusuz <alperen.kapusuz@bulutmd.com>
2023-03-05 00:32:28 +0300

    SearchInput onlyCharacter utils added

--- src/screens/ExploreScreen/SearchInput.js
+++ src/screens/ExploreScreen/SearchInput.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, SafeAreaView, TextInput, FlatList } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import RestaurantCard from '../../components/RestaurantCard';
+import { onlyCharacter } from '../../utils/onlyCharacter'
 
 const SearchInput = (props) => {
     const { goBack, data } = props
@@ -37,7 +38,7 @@ const SearchInput = (props) => {
                 style={{ marginHorizontal: 10, backgroundColor: 'rgb(240, 238, 230)', borderColor: 'rgb(217, 213, 169)', borderWidth: 1, borderColor: 'rgb(237, 176, 7)', borderRadius: 5 }}
                 placeholder='Restoran ara'
                 value={filter}
-                onChangeText={text => setFilter(text)}
+                onChangeText={text => setFilter(onlyCharacter(text))}
                 autoFocus={true}
                 placeholderTextColor={'#000000'}
             />
