a629e73d6f3f563e7e203d011d34c1229ac07072
Alperen Kapusuz <alperen.kapusuz@bulutmd.com>
2023-03-15 19:27:12 +0300

    react-native-dotenv added

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin', 'react-native-paper/babel'],
+  plugins: ['react-native-reanimated/plugin', 'react-native-paper/babel', [
+    'module:react-native-dotenv',
+    {
+      envName: 'APP_ENV',
+      moduleName: '@env',
+      path: '.env',
+    },
+  ],
+  ],
   env: {
     production: {
       plugins: ['react-native-paper/babel'],
